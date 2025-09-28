d79e41a9dfb39fc5422549c44eccf0c656d19ebc
raufzaidee15-design <raufzaidee15@gmail.com>
2025-09-28 11:47:40 +0800

    Removed DataRepository.kt from wrong location

--- app/src/main/java/com/example/mawai/model/app/src/main/java/com/example/mawai/DataRepository.kt
+++
@@ -1,9 +0,0 @@
-package com.example.mawai
-
-import com.example.mawai.model.PenState
-import com.example.mawai.model.MilkingData
-
-object DataRepository {
-    val penStates = mutableListOf<PenState>()
-    val milkingData = mutableListOf<MilkingData>()
-}
