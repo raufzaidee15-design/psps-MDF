0863f93ed2fd06bd042d85d4fa097b48c937b4eb
raufzaidee15-design <raufzaidee15@gmail.com>
2025-09-28 12:14:15 +0800

    Update Navigation.kt

--- app/src/main/java/com/example/mawai/Navigation.kt
+++ app/src/main/java/com/example/mawai/Navigation.kt
@@ -1,24 +1,34 @@
 package com.example.mawai
 
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
 
 @Composable
-fun AppNavigation(modifier: Modifier = Modifier) {
-    val navController = rememberNavController()
-
+fun Navigation(navController: NavHostController, viewModel: DairyViewModel) {
     NavHost(
         navController = navController,
-        startDestination = "main",
-        modifier = modifier
+        startDestination = "main"
     ) {
-        composable("main") { MainScreen(navController) }
-        composable("penstate") { PenStateScreen() }
-        composable("milking") { MilkingScreen() }
-        composable("report") { ReportScreen() }
+        // Main menu screen
+        composable("main") {
+            MainScreen(navController)
+        }
+
+        // Pen State input screen
+        composable("pen_state") {
+            PenStateScreen(viewModel, navController)
+        }
+
+        // Milking Data input screen
+        composable("milking_data") {
+            MilkingDataScreen(viewModel, navController)
+        }
+
+        // Combined report screen
+        composable("report") {
+            ShowReportScreen(viewModel)
+        }
     }
 }
